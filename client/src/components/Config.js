5cb74af26c6d9ec857203cb5fd8578f97d431cf9
leedong2897 <skyblue5030@naver.com>
2021-09-28 01:03:24 +0900

    Lading Page1

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,5 +2,5 @@
 export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
-
+export const API_KEY = 'e428c35b4f5a9c71061fcab7b7ca0a49';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
